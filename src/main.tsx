af32d0207665d46e62fa604e5de428daf84fa2d6
code-acezero <codeacezero@gmail.com>
2026-01-07 18:49:09 +0600

    Add remaining shadcn ui components

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <--- 1. Import this
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  // 2. Wrap your App in BrowserRouter
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
